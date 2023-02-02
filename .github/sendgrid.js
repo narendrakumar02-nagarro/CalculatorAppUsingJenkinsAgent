0b0a0d44bda9ccf7974e9848fa302f49ba8cc679
NARENDRA KUMAR <87057358+narendrakumar02@users.noreply.github.com>
2023-02-02 18:19:49 +0530

    Update sendgrid.js

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -9,7 +9,7 @@ const msg = {
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
+};
 sgMail
   .send(msg)
   .then(() => {console.log('Email sent')})
