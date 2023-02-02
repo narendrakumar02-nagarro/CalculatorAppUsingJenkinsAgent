42e0080d38794d6e2695788b1532c339bf7a3e00
NARENDRA KUMAR <87057358+narendrakumar02@users.noreply.github.com>
2023-02-02 17:55:20 +0530

    Update sendgrid.js

--- .github/sendgrid.js
+++ .github/sendgrid.js
@@ -1,4 +1,23 @@
-#! /usr/bin/env node
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+javascript
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey('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')
+const msg = {
+  to: 'rexzoneandothers@gmail.com', // Change to your recipient
+  from: 'narendra.kumar02@nagarro.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })#! /usr/bin/env node
 
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey('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');
